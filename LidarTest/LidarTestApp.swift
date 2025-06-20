51f0d1963cce4096f8d2614c6c4180a483482354
Hao Mi <mihao_2002@hotmail.com>
2025-06-19 22:03:43 -0700

    Initial Commit

---
+++ LidarTest/LidarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidarTestApp.swift
+//  LidarTest
+//
+//  Created by Hao Mi on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct LidarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
